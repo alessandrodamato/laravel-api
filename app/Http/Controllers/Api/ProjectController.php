4297f9faa03e6d3c0ede69f3984f43bc7574021b
alessandrodamato <alessandro042004@gmail.com>
2024-06-03 15:58:03 +0200

    [add] technologies/types API

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Project;
+use App\Models\Technology;
+use App\Models\Type;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
@@ -11,5 +13,15 @@ class ProjectController extends Controller
     public function index(){
       $projects = Project::with('type', 'technologies')->paginate(12);
       return response()->json($projects);
+    }
+
+    public function getTechnologies(){
+      $technologies = Technology::all();
+      return response()->json($technologies);
+    }
+
+    public function getTypes(){
+      $types = Type::all();
+      return response()->json($types);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/projects', [ProjectController::class, 'index']);
+Route::get('/technologies', [ProjectController::class, 'getTechnologies']);
+Route::get('/types', [ProjectController::class, 'getTypes']);
