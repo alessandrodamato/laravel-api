68a200d2f591cb015a9f270afe2b33e797685e91
alessandrodamato <alessandro042004@gmail.com>
2024-06-04 14:00:27 +0200

    [fix] unique slug

--- database/migrations/2024_05_21_131011_create_projects_table.php
+++ database/migrations/2024_05_21_131011_create_projects_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
             $table->string('name', 100);
-            $table->string('slug', 100);
+            $table->string('slug', 100)->unique();
             $table->string('creator', 100);
             $table->string('objective', 50);
             $table->string('file')->nullable();
